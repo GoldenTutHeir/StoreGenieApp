ee8c4d04ba702ea6647122d3ef173dbf71d9383b
GoldenTutHeir <ani86shende@gmail.com>
2024-08-02 18:49:56 +0530

    test: checking commits

--- src/main/java/com/example/storeGenie/constants/CafeConstants.java
+++ src/main/java/com/example/storeGenie/constants/CafeConstants.java
@@ -5,4 +5,5 @@ public class CafeConstants {
     public static final String SOMETHING_WENT_WRONG="Something Went Wrong";
     public static final String INVALID_DATA="Invalid Data";
     public static final int ERROR_404=404;
+    public static final int test = 10;
 }
