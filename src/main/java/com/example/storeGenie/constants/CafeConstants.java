d7c87eb53d498cb6eecf31e8eaa34f35f350737d
GoldenTutHeir <ani86shende@gmail.com>
2024-08-11 16:47:24 +0530

    upd: updated constants

--- src/main/java/com/example/storeGenie/constants/CafeConstants.java
+++ src/main/java/com/example/storeGenie/constants/CafeConstants.java
@@ -3,7 +3,7 @@ package com.example.storeGenie.constants;
 public class CafeConstants {
 
     public static final String SOMETHING_WENT_WRONG="Something Went Wrong";
+    public static final String UNAUTHORIZED_ACCESS = "unauthorized access.";
     public static final String INVALID_DATA="Invalid Data";
-    public static final int ERROR_404=404;
-    public static final int test = 10;
+    public static final String STORE_LOCATION = "";
 }
