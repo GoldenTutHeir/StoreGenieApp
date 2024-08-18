0229dfc12136dc60557ecd5ba11bd472aa54d768
GoldenTutHeir <ani86shende@gmail.com>
2024-08-18 19:10:12 +0530

    upd: Updated user dao

--- src/main/java/com/example/storeGenie/dao/UserDao.java
+++ src/main/java/com/example/storeGenie/dao/UserDao.java
@@ -1,11 +1,16 @@
 package com.example.storeGenie.dao;
 
 import com.example.storeGenie.POJO.User;
+import com.example.storeGenie.wrapper.UserWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 //we are going to write the implementation of this file in the pojo file
 public interface UserDao extends JpaRepository<User, Integer> {
 
     User findByEmailId(@Param("email") String email);
+
+    List<UserWrapper> getAllUser();
 }
